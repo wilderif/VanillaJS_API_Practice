7001b8efd82b710e8304eb9f042e06e12602cf5e
wilderif <woojuro3@gmail.com>
2024-08-20 14:39:33 +0900

    refactor: Reorganize ‘index.js’ file and add comments
    - Reordered functions and variables in ‘index.js’ for better logical flow.
    - Added comments to the file.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 // jsDoc 주석 추가
 // 전체 로직 설명하는 DOC 잘 작성할 것
 
+// 파일을 기능 단위로 나눌지 컴포넌트 단위로 나눌지 구현 전에 고민하고 시작할 것
+
 import { fetchItemList, fetchItemSearch } from "./api.js";
 import { getStorage } from "./storage.js";
 import {
@@ -9,36 +11,48 @@ import {
   renderPaginationDots,
 } from "./render.js";
 
+/**
+ * DOM Elements
+ */
+// logo element
+const logoEl = document.getElementById("logo");
+// search bar elements
+const searchBarEl = document.getElementById("search-bar");
+const searchBtnEl = document.getElementById("search-btn");
+const searchKeywordEl = document.getElementById("search-keyword-text");
+// modal elements
 const wishListBtnEl = document.getElementById("wish-list-btn");
 const wishListModalContainerEl = document.getElementById(
   "wish-list-modal-container"
 );
-
-const logoEl = document.getElementById("logo");
-
 const modalCloseBtnEl = document.getElementById("modal-close-btn");
-
-const searchKeywordEl = document.getElementById("search-keyword-text");
-
-const searchBarEl = document.getElementById("search-bar");
-const searchBtnEl = document.getElementById("search-btn");
-
+// pagination elements
 const pagePrevBtnEl = document.getElementById("page-prev-btn");
 const pageNextBtnEl = document.getElementById("page-next-btn");
 const pageNumbersEl = document.getElementById("page-numbers");
 
-// -1: fetchItemList
-//  0: fetchItemSearch(search keyword 사용한 검색)
-//  1: wish list 검색 (모달 닫았을 때 like 정보를 동기화하기 위하여, isModalOpen으로 대체)
-export let curSearchType = -1;
+/**
+ * Global Variables
+ */
+/**
+ * curSearchType:
+ *   -1: fetchItemList
+ *    0: fetchItemSearch(search keyword 사용한 검색)
+ *    1: wish list 검색 (모달 닫았을 때 like 정보를 동기화하기 위하여, isModalOpen으로 대체)
+ */
+let curSearchType = -1;
 let isModalOpen = false;
-
 let currentPage = 1;
 let currentTotalResults;
 let currentTotalPages;
 let currentPageData;
 let searchKeyword = "";
 
+/**
+ * displayBookList()
+ * 화면에 나타날 책 목록을 순회하며, renderBook() 함수를 통해 생성
+ * isModalOpen 상태에 때라 data와 render 위치가 달라짐
+ */
 const displayBookList = () => {
   let toRenderData = isModalOpen ? getStorage("wishList") : currentPageData;
   const whereToRender = isModalOpen
@@ -51,30 +65,13 @@ const displayBookList = () => {
   });
 };
 
-export const handlePagination = async (targetPage) => {
-  if (curSearchType === -1) {
-    [currentTotalResults, currentPageData] = await fetchItemList(
-      "ItemNewSpecial",
-      targetPage,
-      8,
-      "Book"
-    );
-  } else if (curSearchType === 0) {
-    [currentTotalResults, currentPageData] = await fetchItemSearch(
-      searchKeyword,
-      "Title",
-      targetPage,
-      8,
-      "Book"
-    );
-  }
-
-  currentPage = targetPage;
-
-  displayBookList();
-  displayPagination(currentPage, currentTotalPages);
-};
-
+/**
+ * displayPagination()
+ * curPage와 totalPages를 받아 페이지네이션을 렌더링
+ * totalPage와 curPage에 따라 페이지네이션을 다르게 처리
+ * renderPaginationNum()과 renderPaginationDots() 함수를 통해 생성
+ * pagenation의 prev, next 버튼에, 조건에 맞게 event listener 추가
+ */
 const displayPagination = (curPage, totalPages) => {
   pageNumbersEl.innerHTML = "";
 
@@ -99,9 +96,8 @@ const displayPagination = (curPage, totalPages) => {
     pageNextBtnEl.style.opacity = "0.5";
     pageNextBtnEl.onclick = null;
   }
-
+  // 총 페이지 수가 5 이하일 때 모든 페이지 번호를 표시
   if (totalPages <= 5) {
-    // 총 페이지 수가 5 이하일 때 모든 페이지 번호를 표시
     for (let i = 1; i <= totalPages; i++) {
       renderPaginationNum(curPage, i);
     }
@@ -132,6 +128,42 @@ const displayPagination = (curPage, totalPages) => {
   }
 };
 
+/**
+ * handlePagination()
+ * 페이지네이션 클릭 시, 해당 페이지의 데이터를 fetch
+ * curSearchType에 따라 fetchItemList() 또는 fetchItemSearch() 호출
+ * fetch 완료되면 displayBookList()와 displayPagination()를 통해 화면에 렌더링
+ */
+export const handlePagination = async (targetPage) => {
+  if (curSearchType === -1) {
+    [currentTotalResults, currentPageData] = await fetchItemList(
+      "ItemNewSpecial",
+      targetPage,
+      8,
+      "Book"
+    );
+  } else if (curSearchType === 0) {
+    [currentTotalResults, currentPageData] = await fetchItemSearch(
+      searchKeyword,
+      "Title",
+      targetPage,
+      8,
+      "Book"
+    );
+  }
+
+  currentPage = targetPage;
+
+  displayBookList();
+  displayPagination(currentPage, currentTotalPages);
+};
+
+/**
+ * handleSubmit()
+ * 검색어를 받아 fetchItemSearch() 함수를 통해,
+ * 화면에 출력될 currentPageData와 pagination을 위한 currentTotalResults를 업데이트
+ * fetch가 완료되면 displayBookList()와 displayPagination()을 통해 화면에 렌더링
+ */
 const handleSubmit = async () => {
   searchKeyword = searchBarEl.value.trim();
 
@@ -161,25 +193,58 @@ const handleSubmit = async () => {
   displayPagination(1, currentTotalPages);
 };
 
+/**
+ * handleCloseModal()
+ * modal을 닫을 때, modal 관련 style을 초기화
+ * isModalOpen false로 변경
+ * modal에서 like 버튼 클릭 했을 때, 동기화하기 위하여
+ * displayBookList()와 displayPagination()을 통해 현재 페이지 다시 화면에 렌더링
+ */
 const handleCloseModal = () => {
   wishListModalContainerEl.classList.remove("active");
-  document.body.style.overflow = ""; // 모달 닫힐 때 배경 스크롤 해제
+  document.body.style.overflow = ""; // 모달 닫힐 때 배경 스크롤 잠금 해제
   isModalOpen = false;
   displayBookList();
   displayPagination(currentPage, currentTotalPages);
 };
 
+/**
+ * init()
+ * 초기화, Logo 클릭 시 호출
+ * ItemNewSpecial를 통해 "주목할 만한 신간 리스트"를 fetch
+ * fetch가 완료되면 displayBookList()와 displayPagination()을 통해 화면에 렌더링
+ */
+const init = async () => {
+  [currentTotalResults, currentPageData] = await fetchItemList(
+    "ItemNewSpecial",
+    1,
+    8,
+    "Book"
+  );
+  searchKeywordEl.innerText = "주목할 만한 신간 리스트";
+  currentTotalPages = Math.ceil(currentTotalResults / 8);
+  curSearchType = -1;
+  displayBookList();
+  displayPagination(1, currentTotalPages);
+};
+
+/**
+ * Event Listener
+ */
+
+// search bar event listener
 searchBarEl.addEventListener("keydown", (event) => {
   if (event.key === "Enter" && event.isComposing === false) {
     handleSubmit();
   }
 });
 
-searchBtnEl.addEventListener("click", (event) => {
+searchBtnEl.addEventListener("click", () => {
   handleSubmit();
 });
 
-wishListBtnEl.addEventListener("click", (event) => {
+// Modal Event Listener
+wishListBtnEl.addEventListener("click", () => {
   wishListModalContainerEl.classList.add("active");
   document.body.style.overflow = "hidden"; // 모달 열릴 때 배경 스크롤 잠금
   isModalOpen = true;
@@ -205,20 +270,6 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
-const init = async () => {
-  [currentTotalResults, currentPageData] = await fetchItemList(
-    "ItemNewSpecial",
-    1,
-    8,
-    "Book"
-  );
-  searchKeywordEl.innerText = "주목할 만한 신간 리스트";
-  currentTotalPages = Math.ceil(currentTotalResults / 8);
-  curSearchType = -1;
-  displayBookList();
-  displayPagination(1, currentTotalPages);
-};
+logoEl.addEventListener("click", init);
 
 // init();
-
-logoEl.addEventListener("click", init);
